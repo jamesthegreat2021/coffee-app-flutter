1bf07aec659bc577ab339cd2f900bdef78a22254
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-22 12:58:08 +0300

    adding the textenditngcontroller effects

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/homepage.dart';
-import 'package:flutter_application_1/other_pages.dart';
-import 'package:flutter_application_1/profile_page.dart';
-import 'package:flutter_application_1/settings.dart';
-
-
+import 'package:flutter_application_1/textpage.dart';
 
 void main(){
-  runApp(MaterialApp(
-
-    debugShowCheckedModeBanner: false,
-
-    home: FirstPage(),
-    
-    routes: {
-
-
-      '/thepage': (context) => HomePage(), 
-
-      '/settingspage':  (context) => Settings(), 
-
-      '/profilepage': (context) {
-        return ProfilePage();
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterIncrement(),
+      routes: {
+        '/textpage' : (context) {
+          return Textpage(); 
+        }
       },
-      '/otherpage': (context) {
-        return OtherPage();
-      }
-
-   
-
-    },
-  ));
+    ),
+  );
 }
 
-class FirstPage extends StatefulWidget {
-  const FirstPage({super.key});
+class CounterIncrement extends StatefulWidget {
+
+  const CounterIncrement({super.key});
 
   @override
-  State<FirstPage> createState() => _FirstPageState();
+  State<CounterIncrement> createState() => _CounterIncrementState();
 }
 
-class _FirstPageState extends State<FirstPage> {
- int _selectedIndex = 0;
-
- final List _pages = [
-    HomePage(),
-    ProfilePage(), 
-    Settings(),
-  ]; 
-
-  void changeIndex(int index){
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class _CounterIncrementState extends State<CounterIncrement> {
+  int counter = 0; 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('the first page'),),
-      body: _pages[_selectedIndex],
-      drawer: Drawer(
-        backgroundColor: Colors.deepPurple[100],
-        child: Column(
-          children: [
-            DrawerHeader(child: Icon(Icons.favorite, size: 48)), 
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('H O M E'),
-              onTap: (){
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/thepage');
-              },
-            ), 
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("S E T T I N G S"),
-              onTap: (){
-                Navigator.pop(context); 
-                Navigator.pushNamed(context, '/settingspage');
-              },
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
 
-            ),
-            ListTile(
-              leading: Icon(Icons.arrow_forward),
-              title: Text('O T H E R  P A G E S'),
-              onTap: (){
+          Center(child: Text('you have pressed this $counter times'),),
+          ElevatedButton(
+            onPressed: (){
 
-                Navigator.pop(context); 
-                Navigator.pushNamed(context, '/otherpage'); 
-
-              },
+              Navigator.pushNamed(context, '/textpage');
               
+            },
+            child: Text('Next Page'), 
             ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: changeIndex,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            counter = counter - 1 ;
+          });
+        }, 
+        child: Text('+'),
+        ),
     );
+    
   }
 }
